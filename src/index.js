0d102c6c778443607af3f9b21d3b75515c959242
jpdf00 <j.pdf00@gmail.com>
2021-04-22 13:07:43 -0300

    Add event listeners and factory constructors for tasks and projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("It works?")
+const taskList = [];
+const projectList = [];
+
+const taskSubmit = document.querySelector('#taskSubmit');
+taskSubmit.addEventListener('click', () => {
+  const name = document.getElementById('taskName').value;
+  const description = document.getElementById('taskDescription').value;
+  const priority = document.getElementById('taskPriority').value;
+  const project = document.getElementById('taskProject').value;
+  const dueDate = document.getElementById('taskDueDate').value;
+  addTask(name, description, priority, project, dueDate);
+});
+
+function addTask(name, description, priority, project, dueDate) {
+  const task = Task(name, description, priority, project, dueDate);
+  taskList.push(task);
+};
+
+const Task = (name, description, priority, project, dueDate) => {
+  return {name, description, priority, project, dueDate};
+};
+
+const projectSubmit = document.querySelector('#projectSubmit');
+projectSubmit.addEventListener('click', () => {
+  const name = document.getElementById('projectName').value;
+  addTask(name);
+});
+
+function addTask(name) {
+  const project = Task(name);
+  projectList.push(project);
+};
+
+const Project = (name) => {
+  return {name};
+};
