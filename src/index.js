7544af5cf1f515628ec0223a2a7b1aef83290b28
jpdf00 <j.pdf00@gmail.com>
2021-04-22 13:54:12 -0300

    Add draw view for the tasks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const taskList = [];
 const projectList = [];
 
 const taskSubmit = document.querySelector('#taskSubmit');
+
+function addTask(name, description, priority, project, dueDate) {
+  const task = Task(name, description, priority, project, dueDate);
+  taskList.push(task);
+};
+
 taskSubmit.addEventListener('click', () => {
   const name = document.getElementById('taskName').value;
   const description = document.getElementById('taskDescription').value;
@@ -9,24 +15,22 @@ taskSubmit.addEventListener('click', () => {
   const project = document.getElementById('taskProject').value;
   const dueDate = document.getElementById('taskDueDate').value;
   addTask(name, description, priority, project, dueDate);
+  drawTask(taskList)
 });
-
-function addTask(name, description, priority, project, dueDate) {
-  const task = Task(name, description, priority, project, dueDate);
-  taskList.push(task);
-};
 
 const Task = (name, description, priority, project, dueDate) => {
   return {name, description, priority, project, dueDate};
 };
 
+
+
 const projectSubmit = document.querySelector('#projectSubmit');
 projectSubmit.addEventListener('click', () => {
   const name = document.getElementById('projectName').value;
-  addTask(name);
+  addProject(name);
 });
 
-function addTask(name) {
+function addProject(name) {
   const project = Task(name);
   projectList.push(project);
 };
@@ -34,3 +38,37 @@ function addTask(name) {
 const Project = (name) => {
   return {name};
 };
+
+const TaskHTML = (task) => {
+  const taskItem = document.createElement('div');
+  taskItem.setAttribute('class', 'd-flex flex-column');
+
+  const itemName = document.createElement('h2');
+
+  const itemBottom = document.createElement('div');
+  itemBottom.setAttribute('class', 'd-flex justify-content-between');
+
+  const itemDescription = document.createElement('p');
+  const itemDueDate = document.createElement('p');
+
+  itemName.textContent = task.name;
+  itemDescription.textContent = task.description;
+  itemDueDate.textContent = task.dueDate;
+
+  itemBottom.appendChild(itemDescription);
+  itemBottom.appendChild(itemDueDate);
+
+  taskItem.appendChild(itemName);
+  taskItem.appendChild(itemBottom);
+
+  return { taskItem };
+};
+
+function drawTask(taskList) {
+  const taskView = document.querySelector('#taskView');
+
+  for (let i = 0; i < taskList.length; i += 1) {
+    const task = TaskHTML(taskList[i])
+    taskView.appendChild(task.taskItem);
+  }
+}
